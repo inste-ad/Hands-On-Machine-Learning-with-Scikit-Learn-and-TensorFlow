e216be3863a62c790dea2e4d48418a29ac734eb5
liu_home <lys_uestc1@163.com>
2019-03-30 23:27:00 +0800

    testing

--- SGD_GD.py
+++ SGD_GD.py
@@ -18,7 +18,7 @@ for iteration in range(n_iterations):
     gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
     theta = theta - eta * gradients
 print(theta)
-#######
+########
 ######随机梯度下降
 
 n_epochs = 50
