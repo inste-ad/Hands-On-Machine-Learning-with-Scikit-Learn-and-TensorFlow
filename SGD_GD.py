75d192c2c8092540058b8ccfb708c467fa08d953
liu_home <lys_uestc1@163.com>
2019-03-30 23:16:23 +0800

    SGD 的解释

---
+++ SGD_GD.py
@@ -0,0 +1,45 @@
+import numpy as np 
+import pandas as pd
+
+
+
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+X_b = np.c_[np.ones((100, 1)), X]
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
+##########GD 梯度下降
+eta = 0.1 # learning rate
+n_iterations = 1000
+m = 100
+theta = np.random.randn(2,1) # random initialization
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+print(theta)
+#######
+######随机梯度下降
+
+n_epochs = 50
+t0, t1 = 5, 50 # learning schedule hyperparameters
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1) # random initialization
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+'''
+skelearn 实现
+'''
+'''
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+'''
