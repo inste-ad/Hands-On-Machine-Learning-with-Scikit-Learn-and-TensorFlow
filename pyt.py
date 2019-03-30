01997c9a7408c99bd8da28fceaae26fc584e5685
Liu yangsu <lys_uestc@163.com>
2019-03-30 15:13:00 +0800

    多项式回归，待处理X排序

--- pyt.py
+++ pyt.py
@@ -1,21 +1,34 @@
 
 #%% [markdown]
-# vscode notebook
-#  这是一级标题。
-#  # 这是一个二级标题。
-#  ## 这是一个三级标题
-# ### 这是一个四级标题
-
-
-#%%``
-msg = '12312'
-print(msg)
+## Polynomial Regression
+#  对于非线性模型，线性回归模型就不适用了。SVM，NN等模型处理非线性模型有很好的效果.对于简单的不能不能用一条直线表达的，我们可以用多项式回归。
 
 #%%
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import sklearn
+import matplotlib.pyplot as plt 
 
-ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-ts = ts.cumsum()
-ts.plot()
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+X.sort()
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+plt.scatter(X,y)
+plt.show()
+
+#%% [markdown]
+# 使用PolynomialFeatures 来转换我们的training data，令degree = 2 ，意为二次函数。
+#%%
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+X[0],X_poly[0]
+#%%
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression().fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+#%%
+y_pre = lin_reg.predict(X_poly)
+plt.plot(X, y_pre)
+
