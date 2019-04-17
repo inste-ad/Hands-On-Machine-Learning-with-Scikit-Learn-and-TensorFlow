913b61af12e3411890803b140d6b8bc86411ff36
liu_home <lys_uestc1@163.com>
2019-04-17 22:39:43 +0800

    ·

--- Logistics_regression.py
+++ Logistics_regression.py
@@ -9,8 +9,10 @@ iris = datasets.load_iris()
 X = iris["data"][:,3:]
 y = (iris["target"] == 2).astype(np.int)
 
+#%%[markdown]
+# 代码中用了一个小trick。x\[y==0\].
+# 其实是利用y的排序对X进行挑选。y==0，print之后其实是\[True True False ...\]，那么X[y==0, 0],就X的前三个元素而言，分别代表返回（包含），返回（包含），不返回（不包含）；与之类似对于“iris["data"][:, (2, 3)]”这个写法，左边的那个"："代表的行是[True，True, ... ,True]，全是true；
 #%%
-
 from sklearn.linear_model import LogisticRegression
 log_reg = LogisticRegression()
 log_reg.fit(X, y)
@@ -19,8 +21,8 @@ X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
 y_proba = log_reg.predict_proba(X_new)
 plt.plot(X_new, y_proba[:, 1], "g-", label="Iris-Virginica")
 plt.plot(X_new, y_proba[:, 0], "b--", label="Not Iris-Virginica")
-plt.scatter(X[y==0], y[y==0], "bs")
-plt.scatter(X[y==1], y[y==1], "g^")
+plt.plot(X[y==0], y[y==0], "bs")
+plt.plot(X[y==1], y[y==1], "g^")
 
 #%% [markdown]
 ## Softmax Regression
@@ -35,3 +37,7 @@ clf = LogisticRegression(random_state=0, solver='lbfgs',
 clf.predict(X[:2, :])
 clf.predict_proba(X[:2, :]) 
 clf.score(X, y)
+
+
+
+
